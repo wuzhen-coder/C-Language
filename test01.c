b2742d57c3b40dc31138a08816d647bbab182f99
wuzhen-coder <2269995100@qq.com>
2021-01-29 13:50:43 +0800

    打印100~200之间的素数

--- test01.c
+++ test01.c
@@ -7,17 +7,19 @@ int main()
     int count=0;
     for(i=101;i<=200;i+=2)
     {
-        for(j=2;j<sqrt(i);j++)//12=2*6=3*4=4*6*2
-        {
-            if(i%j==0)//代表可以整除
-                break;
-        }
-		if(j>sqrt(i))
-		{
-			printf("%d ",i);//放在循环之后
-			count++;
-		}
+      for(j=2;j<=sqrt(i);j++)
+      {
+          if(i%j==0)
+          {
+              break;
+          }
+      }
+      if(j>sqrt(i))
+      {
+          printf("%d ",i);
+          count++;
+      }
     }
-	printf("\ncount=%d\n",count);
+    printf("\ncount=%d\n",count); 
     return 0;
 }
