a5c963762181a9c4173070bd46035cb3e5890add
wuzhen-coder <2269995100@qq.com>
2021-02-01 13:30:32 +0800

    逆置数组

---
+++ test18.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void Init(int arr[],int sz)
+{
+    int i=0;
+    for(i=0;i<sz;i++)
+    {
+        arr[i]=0;
+    }
+}
+void Print(int arr[],int sz)
+{
+    int i=0;
+    for(i=0;i<sz;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+void Reverse(int arr[],int sz)
+{
+    int left=0;
+    int right=sz-1;
+    while(left<=right)
+    {
+        int tmp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=tmp;
+        left++;
+        right--;
+    }    
+}
+int main()
+{
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int sz=sizeof(arr)/sizeof(arr[0]);
+    //Init(arr,sz);//把数组初始化0
+    Print(arr,sz);//打印
+    Reverse(arr,sz);//逆序
+    Print(arr,sz);//打印
+    return 0;
+}
