67f53661a19cfd84983872e78ccdfabf8c9bd152
wuzhen-coder <2269995100@qq.com>
2021-02-02 14:46:05 +0800

    Sn=a+aa+aaa+aaaa+aaaaa

---
+++ test22.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    int n=0;
+    scanf("%d%d",&a,&n);
+    int i=0;
+    int sum=0;
+    int ret=0;
+    for(i=0;i<n;i++)
+    {
+        ret=ret*10+a;
+        sum+=ret;
+    }
+    printf("%d\n",sum);
+}
